75a0ffe8c30a6c92b10db4524619c7278109b3dd
Josefina Tovar <josefina10969@gmail.com>
2021-12-05 13:24:09 -0600

    Update structure of SintacticAnalizer

--- src/components/SintacticAnalizer/SintacticAnalizer.js
+++ src/components/SintacticAnalizer/SintacticAnalizer.js
@@ -65,198 +65,198 @@ F->( E )|num`)
         <h1 className="text-gray font-bold text-4xl p-6 text-center">
           Análisis LR(1)
         </h1>
-
-        <div className='flex'>
-          <div className='flex-col w-1/4'>
-            <div className="ml-10">
-              <textarea className='bg-gray-light rounded-sm p-5' rows={10} cols={21} 
-                value={grammar} onChange={e=>setGrammar(e.target.value)} 
-                spellCheck="false"/>
-            </div>
-            <div className="items-end text-right">
-              <Button label="Crear" onClick={analize} className=""/>
-            </div>
-          </div>
-
-          <div className="w-1/5 flex-col pl-6">
-              <h2 className="text-gray font-bold text-xl">
-                No Terminales
-              </h2>
-              <div className="flex flex-col" >
-                {augmentedGrammar && augmentedGrammar.noTerminals?.map((symbol, i)=>{
-                  return (
-                    <div key={i}>
-                      <span >{symbol}</span>
-                    </div>
-                  )
-                })}
+        <section id="bigContainer" className="flex mr-5">
+          <article id="1stHalf" className="flex-col w-1/2">
+            <div id="grammarAndTerminals" className="flex">
+              <div id="grammar" className="flex-col ml-10">
+                <div>
+                  <textarea className='bg-gray-light rounded-sm p-5' rows={10} cols={21} 
+                  value={grammar} onChange={e=>setGrammar(e.target.value)} 
+                  spellCheck="false"/>
+                </div>
+                <div className="items-end text-right">
+                  <Button label="Crear" onClick={analize} className=""/>
+                </div>
               </div>
-              
-              <div className="flex flex-col overflow-y-scroll">
+              <div id="terminals" className="ml-10">
                 <h2 className="text-gray font-bold text-xl">
-                  Terminales
+                  No Terminales
                 </h2>
-                {augmentedGrammar && augmentedGrammar.terminalsStructure?.map((object, i)=>{
-                  return (
-                    <div key={i}>
-                      <span >{object.symbol}</span>
-                      <input type="text" placeholder="token" className="ring-1 ring-gray-middle m-auto p-1 rounded w-32 mx-5" 
-                              onChange={e => setToken(e.target.value, i)} value={object.token}>
-                      </input>
-                    </div>
-                  )
-                })}
+                <div className="flex flex-col" >
+                  {augmentedGrammar && augmentedGrammar.noTerminals?.map((symbol, i)=>{
+                    return (
+                      <div key={i}>
+                        <span >{symbol}</span>
+                      </div>
+                    )
+                  })}
+                </div>
+                
+                <div className="flex flex-col overflow-y-scroll">
+                  <h2 className="text-gray font-bold text-xl">
+                    Terminales
+                  </h2>
+                  {augmentedGrammar && augmentedGrammar.terminalsStructure?.map((object, i)=>{
+                    return (
+                      <div key={i}>
+                        <span >{object.symbol}</span>
+                        <input type="text" placeholder="token" className="ring-1 ring-gray-middle m-auto p-1 rounded w-32 mx-5" 
+                                onChange={e => setToken(e.target.value, i)} value={object.token}>
+                        </input>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div className="items-end text-right">
+                  <Button label="Asignar tokens"/>
+                </div>
+              </div>
+            </div>
+            <div id="sigma" className="flex-col ml-10">
+              <div>
+                <h2 className="text-gray font-bold text-2xl my-5">
+                  Analizar Sintácticamente sigma
+                </h2>
+              </div>
+              <div>
+                <h2 className="text-gray font-bold text-xl py-2">
+                  Seleccionar AFD
+                </h2>
+                <label htmlFor="AFN1" className="text-gray-middle">AFD:</label><br/>
+                <select name="AFN1" className="ring-1 ring-gray-middle m-auto p-1 rounded my-1 w-1/2"
+                        onChange={e => updateAFD(e.target.value)}>
+                        <option value={-1} key={-1}>Selecciona un AFD</option>
+                            {myAFNs.map((element, i)=>{
+                                if(element.afn.isAFD){
+                                    return(
+                                        <option value={i} key={i}>{element.name}</option>
+                                    )
+                                }else return '';
+                            })}
+                </select>
+                <article className="m-auto mb-0 ml-0 py-3">
+                  Sigma<br/>
+                    <input type="text" placeholder="SIGMA" className="ring-1 ring-gray-middle m-auto p-1 rounded" 
+                            onChange={e => setSigma(e.target.value)} value={sigma}>
+                    </input>
+                </article>
+                <Button label="Probar Léxico" onClick={analyzeString} />
+              </div>
+            </div>
+          </article>
+          <article id="2ndHalf" className="flex-col w-1/2">
+            <div id="table1">
+              <h2 className="text-gray font-bold text-xl">
+                Crear Tabla
+              </h2>
+              <div>
+                Tabla LR(1)
               </div>
               <div className="items-end text-right">
-                <Button label="Asignar tokens"/>
-              </div>
-          </div>
-
-          <div className="w-2/5 flex-col pl-6">
-            <h2 className="text-gray font-bold text-xl">
-              Crear Tabla
-            </h2>
-            <div>
-              Tabla LR(1)
-            </div>
-            <div className="items-end text-right">
-              <Button label="Crear Tabla" onClick={analize}/>
-            </div>   
-            <div className="w-auto h-auto grid justify-items-center p-10">
-                <table className="divide-y divide-gray w-full">
-                    <thead >
-                        <tr >
-                            <th className="p-1">No Terminal</th>
-                            {augmentedGrammar && LRTableSymbols.map((symb, i)=>{
-                              return <th key={i} className="px-2 border-2">{symb}</th>
-                            })}
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-middle">
-                        {LRTable.map((obj, i)=>{
-                            return(
-                                <tr key={i}>
-                                  <td className="p-1">{i}</td>
-                                  {augmentedGrammar && LRTableSymbols.map((symb, i)=>{
-                                    let state = ''
-                                    const haveTransition = obj.transitions.concat(obj.reductions).some(element => {
-                                      if(element==null)return
-                                      state = element.state;
-                                      return element.symbol===symb;
-                                    });
-                                    if(haveTransition){
-                                        return <th key={i} className="px-2 border-2">{state}</th>
-                                    }else{
-                                      return <th key={i} className="px-2 border-2"></th>
-                                    }
-                                  })}
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="flex">
-          <div className="ml-10 w-2/6 flex-col">
-            <div>
-              <h2 className="text-gray font-bold text-2xl my-5">
-                Analizar Sintácticamente sigma
-              </h2>
-            </div>
-            <div>
-              <h2 className="text-gray font-bold text-xl py-2">
-                Seleccionar AFD
-              </h2>
-              <label htmlFor="AFN1" className="text-gray-middle">AFD:</label>
-              <select name="AFN1" className="ring-1 ring-gray-middle m-auto p-1 rounded my-1 w-full"
-                      onChange={e => updateAFD(e.target.value)}>
-                      <option value={-1} key={-1}>Selecciona un AFD</option>
-                          {myAFNs.map((element, i)=>{
-                              if(element.afn.isAFD){
-                                  return(
-                                      <option value={i} key={i}>{element.name}</option>
-                                  )
-                              }else return '';
+                <Button label="Crear Tabla" onClick={analize}/>
+              </div>   
+              <div className="w-auto h-auto grid justify-items-center p-10">
+                  <table className="divide-y divide-gray w-full">
+                      <thead >
+                          <tr >
+                              <th className="p-1">No Terminal</th>
+                              {augmentedGrammar && LRTableSymbols.map((symb, i)=>{
+                                return <th key={i} className="px-2 border-2">{symb}</th>
+                              })}
+                          </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-middle">
+                          {LRTable.map((obj, i)=>{
+                              return(
+                                  <tr key={i}>
+                                    <td className="p-1">{i}</td>
+                                    {augmentedGrammar && LRTableSymbols.map((symb, i)=>{
+                                      let state = ''
+                                      const haveTransition = obj.transitions.concat(obj.reductions).some(element => {
+                                        if(element==null)return
+                                        state = element.state;
+                                        return element.symbol===symb;
+                                      });
+                                      if(haveTransition){
+                                          return <th key={i} className="px-2 border-2">{state}</th>
+                                      }else{
+                                        return <th key={i} className="px-2 border-2"></th>
+                                      }
+                                    })}
+                                  </tr>
+                              )
                           })}
-              </select>
-              <article className="m-auto mb-0 ml-0 py-3">
-                Sigma<br/>
-                  <input type="text" placeholder="SIGMA" className="ring-1 ring-gray-middle m-auto p-1 rounded" 
-                          onChange={e => setSigma(e.target.value)} value={sigma}>
-                  </input>
-              </article>
-              <Button label="Probar Léxico" onClick={analyzeString} />
+                      </tbody>
+                  </table>
+              </div>
             </div>
-          </div>
-
-          <div className="w-1/6 h-auto grid justify-items-center p-10">
-            <table className="divide-y divide-gray w-full">
-                <thead >
-                    <tr >
-                        <th className="p-5">Lexema</th>
-                        <th>Token</th>
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-middle">
-                    {lexTable.map((obj, i)=>{
-                        return(
-                            <tr key={i}>
-                                <td className="p-1">{obj[1]}</td>
-                                <td className="p-1">{obj[0]}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-          </div>
-          <div className="w-4/6 h-auto grid justify-items-center overflow-x-scroll">
-              <Button label="Analizar sintácticamente"/>
+            <div id="table2">
+              <div className="w-1/6 h-auto grid justify-items-center p-10">
               <table className="divide-y divide-gray w-full">
                   <thead >
-                      <tr>
-                          <th className="p-1"></th>
-                          <th className="p-1">Pila</th>
-                          <th className="p-1">Cadena</th>
-                          <th className="p-1">Accion</th>
+                      <tr >
+                          <th className="p-5">Lexema</th>
+                          <th>Token</th>
                       </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-middle">
-                      {/*lexTable.map((obj, i)=>{
+                      {lexTable.map((obj, i)=>{
                           return(
                               <tr key={i}>
                                   <td className="p-1">{obj[1]}</td>
                                   <td className="p-1">{obj[0]}</td>
                               </tr>
                           )
-                      })*/}
-                      {
-                          afdTable?.map((element, index) => {
-                              return(
-                                  <tr key={index}>
-                                      <td className="p-2">
-                                          {index}
-                                      </td>
-                                      {
-                                          element?.map((element, index) => {
-                                              return(
-                                                  <td className="p-2" key={index}>
-                                                      {element}
-                                                  </td>
-                                              );
-                                          })
-                                      }
-                                  </tr>
-                              );
-                          })
-                      }
+                      })}
                   </tbody>
               </table>
             </div>
-        </div>  
+            <div className="w-4/6 h-auto grid justify-items-center overflow-x-scroll">
+                <Button label="Analizar sintácticamente"/>
+                <table className="divide-y divide-gray w-full">
+                    <thead >
+                        <tr>
+                            <th className="p-1"></th>
+                            <th className="p-1">Pila</th>
+                            <th className="p-1">Cadena</th>
+                            <th className="p-1">Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-middle">
+                        {/*lexTable.map((obj, i)=>{
+                            return(
+                                <tr key={i}>
+                                    <td className="p-1">{obj[1]}</td>
+                                    <td className="p-1">{obj[0]}</td>
+                                </tr>
+                            )
+                        })*/}
+                        {
+                            afdTable?.map((element, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td className="p-2">
+                                            {index}
+                                        </td>
+                                        {
+                                            element?.map((element, index) => {
+                                                return(
+                                                    <td className="p-2" key={index}>
+                                                        {element}
+                                                    </td>
+                                                );
+                                            })
+                                        }
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+              </div>
+            </div>
+          </article>
+        </section>
       </div>
       <AllAFN/>
     </div>
